05f97b1031a885a614deacd2c869b8c118996124
Qiancheng <occ_759566531@live.com>
2022-09-16 16:33:42 +0800

    Add the main program entry main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+# by:Caiqiancheng
+# Date:2022/9/16
+import configparser
+
+from bin.Generation import generation
+from bin.Generation_ui import generation_ui
+from src.common import read_config
+
+
+def get_start_mode():
+    """
+    从配置文件获取启动方式
+    :return: cmd or gui
+    """
+    start_mode = read_config("start").get("mode")
+    return start_mode
+
+
+if __name__ == '__main__':
+    if get_start_mode() == "cmd":
+        generation()
+    elif get_start_mode() == "gui":
+        generation_ui()
